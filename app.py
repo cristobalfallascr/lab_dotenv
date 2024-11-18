c2bb2a8167519661032865c35e39ba745d0b8f31
Cristobal Fallas <cristobalfallascr@hotmail.com>
2024-11-18 16:57:17 -0600

    inital commit, test files and variables

--- app.py
+++ app.py
@@ -7,8 +7,9 @@ from flask_cors import CORS
 load_dotenv()
 
 print(os.getenv("TEST1"))
+print(os.getenv("TEST_2"))
 
 app = Flask(__name__)
 CORS(app)
 
-app.get("/")(lambda: f"Hello World! {os.getenv("TEST1")}" )
+app.get("/")(lambda: f"Hello World! {os.getenv("TEST1")} and now also {os.getenv("TEST_2")}" )
