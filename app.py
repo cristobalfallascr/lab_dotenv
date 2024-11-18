a28ea0f4a2332374e0d7492e6e47702b110b8f70
Cristobal Fallas <cristobalfallascr@hotmail.com>
2024-11-18 17:02:04 -0600

    inital commit, test files and variables

--- app.py
+++ app.py
@@ -12,4 +12,4 @@ print(os.getenv("TEST_2"))
 app = Flask(__name__)
 CORS(app)
 
-app.get("/")(lambda: f"Hello World! {os.getenv("TEST1")} and now also {os.getenv("TEST_2")}" )
+app.get("/")(lambda: f"Hello World! {os.getenv("TEST1")} and now also {os.getenv("TEST_2")}...." )
